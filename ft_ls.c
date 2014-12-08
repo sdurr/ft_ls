57dcccc3d6dcbeb342005a0d30c3527d7506909c
sdurr <sdurr@e3r1p12.42.fr>
2014-12-08 16:23:45 +0100

    ls

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/08 09:35:51 by sdurr             #+#    #+#             */
-/*   Updated: 2014/12/08 15:16:09 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/12/08 16:23:27 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,26 @@
 #include "libft.h"
 #include <stdio.h>
 
-void	ft_ls(char *argv)
+char	**ft_ls(char **argv)
 {
 	DIR *dirp;
 	struct dirent *read;
-	int i;
+	int j;
 
-	i = 0;
-	if ((dirp = opendir(argv)) != NULL) // ouverture argv[1] pointeur sur flux
+	j = 0;
+	if (!(tab = (char **)malloc(sizeof(char *) * ft_ls_count_files(argv) + 1)))
+		return (NULL);
+	if ((dirp = opendir(".")) != NULL) // ouverture argv[1] pointeur sur flux
 	{
 		while ((read = readdir(dirp)) != NULL) // lecture argv
-			i++;
-			printf("%d\n", i);
+		{
+			if (!(tab[j] = (cahr *)malloc(sizeof(char) * ft_strlen(read -> d_name) + 1)))
+				return (NULL);
+			if (!(tmp[j] = ft_strdup(read -> d_name))) //stock noms fichier ou dossiers dans tableau
+				return (NULL);
+			j++;
+		}
 	}
+	tab[j] = NULL;
+	return (tab);
 }
