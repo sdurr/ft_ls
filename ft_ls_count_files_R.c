69a8c130ba9b37f13aeb99c270b4d4dcb0baae72
sdurr <sdurr@e3r1p12.42.fr>
2014-12-09 15:52:26 +0100

    all

--- ft_ls_count_files_R.c
+++ ft_ls_count_files_R.c
@@ -6,7 +6,7 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/08 16:01:31 by sdurr             #+#    #+#             */
-/*   Updated: 2014/12/09 15:04:24 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/12/09 15:46:39 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int		ft_ls_count_files_R(char **argv)
 	int j;
 	DIR *dirp;
 	struct dirent *read;
+	char *tmp;
 
 	j = 1;
 	i = 0;
@@ -35,13 +36,14 @@ int		ft_ls_count_files_R(char **argv)
 		i = 0;
 		while (argv[j])
 		{
-			if ((dirp = opendir(argv[j])) != NULL)
+			tmp = ft_strdup(argv[j]);
+			while  ((dirp = opendir(tmp)) != NULL)
 			{
 				while ((read = readdir(dirp)) != NULL)
 				{
-					ft_putstr(read -> d_name);
-					ft_putchar ('\n');
-					i++;
+					tmp = ft_strdup(read -> d_name);
+					if (tmp[0] != '.')
+						i++;
 				}
 			}
 			j++;
